9112537895c6d09c7bc4deb8da983be95107a6c3
vldmkr <vladmakaryan@gmail.com>
2020-03-04 02:34:52 +0200

    unnecessary libs for the first release removed

--- lib/Platforms/Sonoff.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define PIN_STATUS_LED      13
-#define PIN_RELAY           12
-#define PIN_BUTTON          0
--- lib/Platforms/WemosMini.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <ConsoleController.h>
-#include <i2c_ssd1306.h>
-
-#define SDA           D2
-#define SCL           D1
