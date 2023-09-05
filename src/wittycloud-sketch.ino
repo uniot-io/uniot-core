12ee93f6ba98563982194ce63a219e4df12e1362
JackDrakkar <jack.d.drakkar@gmail.com>
2023-09-06 01:09:05 +0300

    added NTPClient added Date updated wittycloud-sketch

--- src/wittycloud-sketch.ino
+++ src/wittycloud-sketch.ino
@@ -1,12 +1,15 @@
 #include <AppKit.h>
+#include <Date.h>
 #include <Board-WittyCloud.h>
 #include <LispPrimitives.h>
 #include <Logger.h>
 #include <Uniot.h>
+#include <NTPClient.h>
 
 using namespace uniot;
 
 AppKit MainAppKit(MyCredentials, PIN_BUTTON, BTN_PIN_LEVEL, RED);
+Date date;
 
 auto taskPrintHeap = TaskScheduler::make([&](short t) {
   Serial.println(ESP.getFreeHeap());
@@ -19,11 +22,15 @@ void inject() {
   UniotPinMap.setAnalogInput(1, LDR);
 
   MainEventBus.connect(&MainAppKit);
+  MainEventBus.connect(&date);
+  
   MainScheduler.push(&MainAppKit)
+      ->push(&date)
       ->push(taskPrintHeap);
 
   taskPrintHeap->attach(500);
 
+  date.begin();
   MainAppKit.begin();
 
   UNIOT_LOG_INFO("%s: %s", "CHIP_ID", String(ESP.getChipId(), HEX).c_str());
