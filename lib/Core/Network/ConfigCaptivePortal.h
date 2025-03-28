be1ac76f68d64590bb5aa333768837d191075663
vldmkr <vladmakaryan@gmail.com>
2025-03-28 02:00:20 +0200

    Refactor ConfigCaptivePortal class for improved readability and consistency

--- lib/Core/Network/ConfigCaptivePortal.h
+++ lib/Core/Network/ConfigCaptivePortal.h
@@ -19,78 +19,77 @@
 #pragma once
 
 #if defined(ESP8266)
-    #include "ESP8266WebServer.h"
-    typedef ESP8266WebServer WebServer;
+#include "ESP8266WebServer.h"
+typedef ESP8266WebServer WebServer;
 #elif defined(ESP32)
-    #include <WebServer.h>
+#include <WebServer.h>
 #endif
-#include <DNSServer.h>
 #include <Common.h>
+#include <DNSServer.h>
 #include <IExecutor.h>
 
-#define DNS_PORT      53
-#define HTTP_PORT     80
-#define DOMAIN_NAME   "*"
+#define DNS_PORT 53
+#define HTTP_PORT 80
+#define DOMAIN_NAME "*"
 
 namespace uniot {
-  class ConfigCaptivePortal : public IExecutor
-  {
-  public:
-    ConfigCaptivePortal(const IPAddress &apIp)
-    : mIsStarted(false),
-    mApIp(apIp),
-    mpDnsServer(new DNSServer()),
-    mpWebServer(new WebServer(HTTP_PORT)) {}
+class ConfigCaptivePortal : public IExecutor {
+ public:
+  ConfigCaptivePortal(const IPAddress& apIp)
+      : mIsStarted(false),
+        mApIp(apIp),
+        mpDnsServer(new DNSServer()),
+        mpWebServer(new WebServer(HTTP_PORT)) {}
 
-    bool start() {
-      if(!mIsStarted) {
-        mpDnsServer->setTTL(300);
-        mpDnsServer->setErrorReplyCode(DNSReplyCode::ServerFailure);
-        if(mpDnsServer->start(DNS_PORT, DOMAIN_NAME, mApIp)) {
-          mpWebServer->begin();
-          return mIsStarted = true;
-        }
-        return false;
+  bool start() {
+    if (!mIsStarted) {
+      mpDnsServer->setTTL(300);
+      mpDnsServer->setErrorReplyCode(DNSReplyCode::ServerFailure);
+      if (mpDnsServer->start(DNS_PORT, DOMAIN_NAME, mApIp)) {
+        mpWebServer->begin();
+        return mIsStarted = true;
       }
-      return true;
+      return false;
     }
+    return true;
+  }
 
-    void stop() {
-      if(mIsStarted) {
-        // DNSServer has problem with memory deallocation when stop() called
-        // TODO: fix, pull request
-        mpDnsServer->stop();
-        mpWebServer->stop();
-        mIsStarted = false;
-      }
-    }
-
-    void reset() {
-      mpDnsServer.reset(new DNSServer());
-      mpWebServer.reset(new WebServer(HTTP_PORT));
+  void stop() {
+    if (mIsStarted) {
+      // DNSServer has problem with memory deallocation when stop() called
+      // TODO: fix, pull request
+      mpDnsServer->stop();
+      mpWebServer->stop();
       mIsStarted = false;
     }
+  }
 
-    WebServer* get() {
-      return mpWebServer.get();
+  void reset() {
+    mpDnsServer.reset(new DNSServer());
+    mpWebServer.reset(new WebServer(HTTP_PORT));
+    mIsStarted = false;
+  }
+
+  WebServer* get() {
+    return mpWebServer.get();
+  }
+
+  inline const IPAddress& ip() const {
+    return mApIp;
+  }
+
+  virtual void execute(short _) {
+    if (mIsStarted) {
+      mpDnsServer->processNextRequest();
+      mpWebServer->handleClient();
     }
+  }
 
-    inline const IPAddress& ip() const {
-      return mApIp;
-    }
+ private:
+  bool mIsStarted;
 
-    virtual void execute(short _) {
-      if(mIsStarted) {
-        mpDnsServer->processNextRequest();
-        mpWebServer->handleClient();
-      }
-    }
-
-  private:
-    bool mIsStarted;
-
-    IPAddress mApIp;
-    UniquePointer<DNSServer> mpDnsServer;
-    UniquePointer<WebServer> mpWebServer;
-  };
-}
+  IPAddress mApIp;
+  UniquePointer<DNSServer> mpDnsServer;
+  UniquePointer<WebServer> mpWebServer;
+};
+}  // namespace uniot
