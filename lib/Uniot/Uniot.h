9652e514919a3c6cecdea47e952507e200c970af
vldmkr <vladmakaryan@gmail.com>
2025-06-23 03:51:11 +0300

    feat: implement safe ID-based timer and listener management

--- lib/Uniot/Uniot.h
+++ lib/Uniot/Uniot.h
@@ -27,39 +27,12 @@
 #include <TaskScheduler.h>
 
 class UniotCore {
- protected:
-  class Timer {
-   public:
-    Timer() : mTask(nullptr) {}
-    Timer(uniot::TaskScheduler::TaskPtr task) : mTask(task) {}
-
-    Timer(const Timer&) = delete;
-    Timer& operator=(const Timer&) = delete;
-
-    Timer(Timer&& other) noexcept : mTask(std::move(other.mTask)) {}
-    Timer& operator=(Timer&& other) noexcept {
-      if (this != &other) {
-        mTask = std::move(other.mTask);
-      }
-      return *this;
-    }
-
-    void cancel() {
-      if (mTask) {
-        mTask->detach();
-        mTask.reset();
-      }
-    }
-
-    bool isActive() const {
-      return mTask && mTask->isAttached();
-    }
-
-   private:
-    uniot::TaskScheduler::TaskPtr mTask;
-  };
-
  public:
+  using TimerId = uint32_t;
+  using ListenerId = uint32_t;
+  static constexpr TimerId INVALID_TIMER_ID = 0;
+  static constexpr ListenerId INVALID_LISTENER_ID = 0;
+
   UniotCore() : mScheduler(), mEventBus(FOURCC(main)), mpNetworkControllerConfig(nullptr) {}
 
   void configWiFiResetButton(uint8_t pinBtn, uint8_t activeLevelBtn = LOW, bool registerLispBtn = true) {
@@ -107,30 +80,191 @@ class UniotCore {
     getAppKit().publishLispEvent(eventID, value);
   }
 
-  Timer setInterval(std::function<void()> callback, uint32_t intervalMs, short times = 0) {
-    if (!callback) {
-      return Timer();
-    }
-
-    auto task = uniot::TaskScheduler::make([callback = std::move(callback)](uniot::SchedulerTask&, short) {
-      callback();
-    });
-    mScheduler.push(nullptr, task);
-    task->attach(intervalMs, times);
-    // Return relies on RVO (Return Value Optimization) to construct Timer directly
-    // at the call site, avoiding any move/copy operations even if they're available
-    return Timer(task);
+  template <typename... Pins>
+  void registerLispDigitalOutput(uint8_t first, Pins... pins) {
+    uniot::PrimitiveExpeditor::getRegisterManager().setDigitalOutput(first, pins...);
   }
 
-  Timer setTimeout(std::function<void()> callback, uint32_t timeoutMs) {
+  template <typename... Pins>
+  void registerLispDigitalInput(uint8_t first, Pins... pins) {
+    uniot::PrimitiveExpeditor::getRegisterManager().setDigitalInput(first, pins...);
+  }
+
+  template <typename... Pins>
+  void registerLispAnalogInput(uint8_t first, Pins... pins) {
+    uniot::PrimitiveExpeditor::getRegisterManager().setAnalogInput(first, pins...);
+  }
+
+  template <typename... Pins>
+  void registerLispAnalogOutput(uint8_t first, Pins... pins) {
+    uniot::PrimitiveExpeditor::getRegisterManager().setAnalogOutput(first, pins...);
+  }
+
+  bool registerLispButton(uniot::Button* button, uint32_t id = FOURCC(_btn)) {
+    return uniot::PrimitiveExpeditor::getRegisterManager().link(uniot::primitive::name::bclicked, button, id);
+  }
+
+  bool registerLispObject(const String& primitiveName, uniot::RecordPtr link, uint32_t id = FOURCC(____)) {
+    return uniot::PrimitiveExpeditor::getRegisterManager().link(primitiveName, link, id);
+  }
+
+  TimerId setInterval(std::function<void()> callback, uint32_t intervalMs, short times = 0) {
+    if (!callback) {
+      return INVALID_TIMER_ID;
+    }
+
+    auto id = _generateTimerId();
+    auto task = uniot::TaskScheduler::make([this, id, callback = std::move(callback)](uniot::SchedulerTask&, short remainingTimes) {
+      callback();
+
+      if (!remainingTimes) {
+        mActiveTimers.remove(id);
+      }
+    });
+
+    mActiveTimers.put(id, task);
+    mScheduler.push(nullptr, task);
+    task->attach(intervalMs, times);
+    return id;
+  }
+
+  TimerId setTimeout(std::function<void()> callback, uint32_t timeoutMs) {
     return setInterval(std::move(callback), timeoutMs, 1);
   }
 
-  Timer setImmediate(std::function<void()> callback) {
+  TimerId setImmediate(std::function<void()> callback) {
     return setTimeout(std::move(callback), 1);
   }
 
-  // todo: LED, network, mqtt callbacks
+  bool cancelTimer(TimerId id) {
+    if (id == INVALID_TIMER_ID) {
+      return false;
+    }
+
+    auto task = mActiveTimers.get(id, nullptr);
+    if (task) {
+      task->detach();
+      return mActiveTimers.remove(id);
+    }
+    return false;
+  }
+
+  bool isTimerActive(TimerId id) {
+    if (id == INVALID_TIMER_ID) {
+      return false;
+    }
+
+    auto task = mActiveTimers.get(id, nullptr);
+    return task && task->isAttached();
+  }
+
+  int getActiveTimersCount() const {
+    return mActiveTimers.calcSize();
+  }
+
+  uniot::TaskScheduler::TaskPtr createTask(const char* name, uniot::SchedulerTask::SchedulerTaskCallback callback) {
+    auto task = uniot::TaskScheduler::make(std::move(callback));
+    mScheduler.push(name, task);
+    return task;
+  }
+
+  template <typename... Topics>
+  ListenerId addSystemListener(std::function<void(unsigned int, int)> callback, unsigned int firstTopic, Topics... otherTopics) {
+    if (!callback) {
+      return INVALID_LISTENER_ID;
+    }
+
+    unsigned int topics[] = {firstTopic, static_cast<unsigned int>(otherTopics)...};
+    size_t count = sizeof...(otherTopics) + 1;
+
+    auto listener = uniot::MakeShared<uniot::CoreCallbackEventListener>(callback);
+    for (size_t i = 0; i < count; ++i) {
+      listener->listenToEvent(topics[i]);
+    }
+
+    if (mEventBus.registerEntity(listener.get())) {
+      auto id = _generateListenerId();
+      mActiveListeners.put(id, listener);
+      return id;
+    }
+
+    return INVALID_LISTENER_ID;
+  }
+
+  bool removeSystemListener(ListenerId id) {
+    if (id == INVALID_LISTENER_ID) {
+      return false;
+    }
+
+    auto listener = mActiveListeners.get(id, nullptr);
+    if (listener) {
+      mEventBus.unregisterEntity(listener.get());
+      return mActiveListeners.remove(id);
+    }
+    return false;
+  }
+
+  template <typename... Topics>
+  size_t removeSystemListeners(unsigned int firstTopic, Topics... otherTopics) {
+    unsigned int topics[] = {firstTopic, static_cast<unsigned int>(otherTopics)...};
+    size_t topicCount = sizeof...(otherTopics) + 1;
+    size_t removedCount = 0;
+
+    mActiveListeners.begin();
+    while (!mActiveListeners.isEnd()) {
+      auto listener = mActiveListeners.current().second;
+      bool shouldRemove = false;
+
+      if (listener) {
+        for (size_t i = 0; i < topicCount; ++i) {
+          if (listener->isListeningToEvent(topics[i])) {
+            shouldRemove = true;
+            break;
+          }
+        }
+      }
+
+      if (shouldRemove) {
+        mEventBus.unregisterEntity(listener.get());
+        mActiveListeners.deleteCurrent();
+        removedCount++;
+      } else {
+        mActiveListeners.next();
+      }
+    }
+
+    return removedCount;
+  }
+
+  bool isSystemListenerActive(ListenerId id) {
+    if (id == INVALID_LISTENER_ID) {
+      return false;
+    }
+
+    return mActiveListeners.exist(id);
+  }
+
+  int getActiveListenersCount() const {
+    return mActiveListeners.calcSize();
+  }
+
+  void emitSystemEvent(unsigned int topic, int message) {
+    mEventBus.emitEvent(topic, message);
+  }
+
+  ListenerId addWifiStatusLedListener(std::function<void(bool)> callback) {
+    if (!callback) {
+      return INVALID_LISTENER_ID;
+    }
+
+    return addSystemListener(
+      [callback = std::move(callback)](unsigned int topic, int message) {
+        if (topic == uniot::NetworkController::WIFI_STATUS_LED) {
+          callback(message != 0);
+        }
+      },
+      uniot::NetworkController::WIFI_STATUS_LED);
+  }
 
   void begin(uint32_t eventBusTaskPeriod = 10) {
     UNIOT_LOG_SET_READY();
@@ -171,7 +305,7 @@ class UniotCore {
  private:
   void _createNetworkControllerConfig() {
     if (!mpNetworkControllerConfig) {
-      mpNetworkControllerConfig = new uniot::AppKit::NetworkControllerConfig();
+      mpNetworkControllerConfig = uniot::MakeUnique<uniot::AppKit::NetworkControllerConfig>();
       mpNetworkControllerConfig->pinBtn = UINT8_MAX;  // Not used by default
       mpNetworkControllerConfig->activeLevelBtn = LOW;
       mpNetworkControllerConfig->pinLed = UINT8_MAX;  // Not used by default
@@ -184,14 +318,25 @@ class UniotCore {
 
   void _deleteNetworkControllerConfig() {
     if (mpNetworkControllerConfig) {
-      delete mpNetworkControllerConfig;
-      mpNetworkControllerConfig = nullptr;
+      mpNetworkControllerConfig.reset(nullptr);
     }
+  }
+
+  TimerId _generateTimerId() {
+    static TimerId nextId = 1;
+    return nextId++;
+  }
+
+  ListenerId _generateListenerId() {
+    static ListenerId nextId = 1;
+    return nextId++;
   }
 
   uniot::TaskScheduler mScheduler;
   uniot::CoreEventBus mEventBus;
-  uniot::AppKit::NetworkControllerConfig* mpNetworkControllerConfig;
+  uniot::Map<TimerId, uniot::TaskScheduler::TaskPtr> mActiveTimers;
+  uniot::Map<ListenerId, uniot::SharedPointer<uniot::CoreCallbackEventListener>> mActiveListeners;
+  uniot::UniquePointer<uniot::AppKit::NetworkControllerConfig> mpNetworkControllerConfig;
 };
 
 extern UniotCore Uniot;
